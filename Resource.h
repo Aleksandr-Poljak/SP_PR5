a9657c6cad9017b09f7141bc11f06eec587a0ebb
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-08-12 04:19:06 +0300

    Added menu items "File", "Viewer" "Help". Added "Exit" command to "File", "Text", "Controls" commands to "View". In the WM_COMMAND handler added processing and stopping of these messages. Added application icon.

--- Resource.h
+++ Resource.h
@@ -1,30 +1,35 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Включаемый файл, созданный в Microsoft Visual C++.
 // Используется SP_PR5.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_SPPR5_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_SPPR5			107
-#define IDI_SMALL				108
-#define IDC_SPPR5			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// Следующие стандартные значения для новых объектов
 //
+#define IDC_MYICON                      2
+#define IDD_SPPR5_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDC_SPPR5                       109
+#define IDR_MAINFRAME                   128
+#define IDI_ICON1                       136
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define IDM_VIEW_TEXT                   32774
+#define ID_32775                        32775
+#define IDM_VIEW_CTL                    32776
+#define ID_ACCELERATOR32777             32777
+#define ID_32780                        32780
+#define IDM_FILE_EXIT                   32781
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        139
+#define _APS_NEXT_COMMAND_VALUE         32782
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
--- wndProcFuncs.cpp
+++ wndProcFuncs.cpp
@@ -24,7 +24,18 @@ void wndProc_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 
     switch (id)
     {
+    case IDM_FILE_EXIT:
+        MessageBox(hwnd, TEXT("Выбрана команда \"Exit\tAlt+x\""), TEXT("Выход"), MB_OK);
+        DestroyWindow(hwnd);
+        break;
+    case IDM_VIEW_TEXT:
+        MessageBox(hwnd, TEXT("Выбрана команда \"ТЕКСТ\""), TEXT("ТЕКСТ"), MB_OK);
+        break;
+    case IDM_VIEW_CTL:
+        MessageBox(hwnd, TEXT("Выбрана команда \"Элементы управления\""), TEXT("ТЕКСТ"), MB_OK);
+        break;
     case IDM_ABOUT:
+        MessageBox(hwnd, TEXT("Выбрана команда \"About\""),TEXT("IDM_ABOUT"), MB_OK);
         DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hwnd, About);
         break;
     case IDM_EXIT:
